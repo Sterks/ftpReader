ffd99cb9f0a2ce7e235e467e0c02d2854e4769e0
Denis S. Runov <it-saratov@mail.ru>
2019-07-31 18:41:01 +0300

    Рекурсивный перебор файлов

--- ftp.go
+++ ftp.go
@@ -3,24 +3,47 @@ package main
 import (
 	"fmt"
 	"github.com/secsy/goftp"
+	"os"
+	"path"
+	"path/filepath"
+	"sync/atomic"
+	"time"
 )
 
-func main(){
+func main() {
 	connect := connect()
-	path := "/fcs_regions/Moskva/notifications"
-	recFiles(connect, path)
+	pathRoot := "/fcs_regions/Moskva/notifications"
+	//recFiles(connect, pathRoot)
+	_ = Walk(connect, pathRoot, func(fullPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			// no permissions is okay, keep walking
+			if err.(goftp.Error).Code() == 550 {
+				return nil
+			}
+			return err
+		}
+
+		fmt.Println(info.Name(), info.Size(), fullPath)
+
+		return nil
+	})
 }
 
 func connect() *goftp.Client {
-	config := goftp.Config{User: "free", Password: "free"}
-	ftp, err := goftp.DialConfig(config,"ftp.zakupki.gov.ru:21")
-	if err != nil{
+	config := goftp.Config{
+		User:               "free",
+		Password:           "free",
+		ConnectionsPerHost: 10,
+		Timeout:            10 * time.Second,
+	}
+	ftp, err := goftp.DialConfig(config, "ftp.zakupki.gov.ru:21")
+	if err != nil {
 		_ = fmt.Errorf("Блок - 1 %v", err)
 	}
 	return ftp
 }
 
-func recFiles(connect *goftp.Client, path string ){
+func recFiles(connect *goftp.Client, path string) {
 
 	list, err := connect.ReadDir(path)
 	if err != nil {
@@ -28,7 +51,54 @@ func recFiles(connect *goftp.Client, path string ){
 	}
 	for _, value := range list {
 		if value.IsDir() == false {
+			from, _ := time.Parse("2006-01-02 15:04:05", "2019-03-01 00:00:00")
+			to, _ := time.Parse("2006-01-02 15:04:05", "2019-04-10 00:00:00")
+			if value.ModTime().After(from) && value.ModTime().Before(to) {
 
+			}
 		}
 	}
+}
+func Walk(client *goftp.Client, root string, walkFn filepath.WalkFunc) (ret error) {
+	dirsToCheck := make(chan string, 100)
+
+	var workCount int32 = 1
+	dirsToCheck <- root
+
+	for dir := range dirsToCheck {
+		go func(dir string) {
+			files, err := client.ReadDir(dir)
+
+			if err != nil {
+				if err = walkFn(dir, nil, err); err != nil && err != filepath.SkipDir {
+					ret = err
+					close(dirsToCheck)
+					return
+				}
+			}
+
+			for _, file := range files {
+				if err = walkFn(path.Join(dir, file.Name()), file, nil); err != nil {
+					if file.IsDir() && err == filepath.SkipDir {
+						continue
+					}
+					ret = err
+					close(dirsToCheck)
+					return
+				}
+
+				if file.IsDir() {
+					atomic.AddInt32(&workCount, 1)
+					dirsToCheck <- path.Join(dir, file.Name())
+				}
+			}
+
+			atomic.AddInt32(&workCount, -1)
+			if workCount == 0 {
+				close(dirsToCheck)
+			}
+		}(dir)
+	}
+
+	return ret
 }
